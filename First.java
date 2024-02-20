9d24798d6cf77252b2499e68825c6b9e1eb1e1d7
Bharanisrii <160491019+Bharanisrii@users.noreply.github.com>
2024-02-20 08:38:47 +0530

    Add files via upload

---
+++ First.java
@@ -0,0 +1,14 @@
+package program;
+
+public class First{
+	
+public static void main(String[]args) {
+	
+
+	
+	int x=5,y=2;
+	boolean z=x-- > 4 || ++y<2;
+	System.out.println(x + " " + y );
+	
+}
+}
